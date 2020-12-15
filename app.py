f770af9a746fba0d66d6844d32591dbaec49f539
Gino <ginohagad@gmail.com>
2020-12-15 22:48:10 +1300

    Created base HTML file.

--- app.py
+++ app.py
@@ -5,16 +5,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Index page'
+    return render_template('home.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-
-    if request.method == 'POST':
-        if valid_login(request.form['username'], request.form['password']):
-            return log_the_user_in(request.form['username'])
-        else:
-            error = 'Invalid username/password'
-
-    return render_template('login.html', error=error)
