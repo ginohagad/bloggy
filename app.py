9e789e6eef7c339f539e967c3170436bb6aa667d
Gino <ginohagad@gmail.com>
2020-12-16 23:51:20 +1300

    Created navbar and home and about page with dummy text.

--- app.py
+++ app.py
@@ -3,7 +3,17 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
